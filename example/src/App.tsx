01180cdfecabe4c386aacbbffcda861b873bfb83
Rithick Jaluthria <50109647+arcade-1024@users.noreply.github.com>
2020-10-14 16:17:33 +0530

    intital commit button {gradient,outline basic}

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
-import { ExampleComponent } from 'button'
+import { ButtonComponent } from 'button'
 import 'button/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <ButtonComponent
+      text='Create React Library Example 😄'
+      textColor={'#000'}
+      textSize={18}
+      textWeight={800}
+      bgColor={'#f4a261'}
+      isShadow={false}
+      isGradient={false}
+      isRounded={true}
+      isOutlined={true}
+      shadowColor={'#48cae4'}
+      gradColor1={'#2a9d8f'}
+      gradColor2={'#48cae4'}
+      gradDirection={'right'}
+    />
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,103 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
+//@ts-ignore
+import styled from 'styled-components'
+
 interface Props {
   text: string
+  bgColor: string
+  textColor: string
+  textWeight: number
+  textSize: number
+  isShadow: boolean
+  shadowColor: string
+  isGradient: boolean
+  isOutlined: boolean
+  isRounded: boolean
+  gradColor1: string
+  gradColor2: string
+  gradDirection: string
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ButtonComponent = ({
+  text,
+  textColor,
+  bgColor,
+  textWeight,
+  textSize,
+  isShadow,
+  shadowColor,
+  isRounded,
+  isGradient,
+  isOutlined,
+  gradColor1,
+  gradColor2,
+  gradDirection
+}: Props) => {
+  let btnDesign = null
+
+  const BasicButton = styled.button`
+    color: ${textColor};
+    background-color: ${bgColor};
+    font-weight: ${textWeight};
+    font-size: ${textSize}px;
+  `
+  const GradientBtn = styled.button`
+    color: ${textColor};
+    font-weight: ${textWeight};
+    font-size: ${textSize}px;
+    background: linear-gradient(
+      to ${gradDirection},
+      ${gradColor1},
+      ${gradColor2}
+    );
+  `
+
+  const OutlinedBtn = styled.button`
+    color: ${textColor};
+    font-weight: ${textWeight};
+    font-size: ${textSize}px;
+    border: 2px solid ${textColor};
+  `
+  const shadow = { boxShadow: `0 5px 10px ${shadowColor}` }
+  const noShadow = { boxShadow: `none` }
+  {
+    isGradient
+      ? (btnDesign = (
+          <GradientBtn
+            className={`${styles.btnBasic}  ${
+              isRounded ? styles.rounded : ' '
+            }`}
+            style={isShadow ? shadow : noShadow}
+          >
+            {text}
+          </GradientBtn>
+        ))
+      : (btnDesign = (
+          <BasicButton
+            className={`${styles.btnBasic} ${
+              isRounded ? styles.rounded : ' '
+            }`}
+            style={isShadow ? shadow : noShadow}
+          >
+            {text}
+          </BasicButton>
+        ))
+  }
+  {
+    isOutlined
+      ? (btnDesign = (
+          <OutlinedBtn
+            className={`${styles.btnBasic}  ${
+              isRounded ? styles.rounded : ' '
+            } `}
+            style={isShadow ? shadow : noShadow}
+          >
+            {text}
+          </OutlinedBtn>
+        ))
+      : ' '
+  }
+  return <div className='styles.container-div'>{btnDesign}</div>
 }
